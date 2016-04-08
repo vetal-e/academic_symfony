5341d508ce53cf5d1c478252b7f0cdbe8eeaf7ef
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-04-08 13:36:52 +0300

    OOT-1375 Additional components and parameters for MopaBootstrapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,10 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new TrackerBundle\TrackerBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Craue\FormFlowBundle\CraueFormFlowBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
