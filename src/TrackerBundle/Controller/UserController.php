7c6d6d5ddb7044d9ed22188bc66cfadbf0efcb82
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-04-11 20:09:29 +0300

    OOT-1349 Backend and frontend for editing user profile

--- src/TrackerBundle/Controller/UserController.php
+++ src/TrackerBundle/Controller/UserController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use TrackerBundle\Form\UserType;
 use TrackerBundle\Entity\User;
 
 class UserController extends Controller
@@ -34,6 +36,42 @@ class UserController extends Controller
         return [
             'user' => $user,
             'roles' => implode(', ', $user->getRoleNames()),
+        ];
+    }
+
+    /**
+     * @Route("/user/edit/{userId}", name="user_edit", requirements={"userId": "\d+"})
+     * @Method({"GET", "POST"})
+     * @Template("user/edit.html.twig")
+     *
+     * @param number $userId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request, $userId = null)
+    {
+        /** @var User $user */
+        if (empty($userId)) {
+            $user = $this->getUser();
+        } else {
+            $user = $this->getDoctrine()->getRepository('TrackerBundle:User')->findOneById($userId);
+        }
+
+        if (empty($user)) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $form = $this->createForm(new UserType(), $user, ['label' => 'Edit user info']);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_view', ['userId' => $user->getId()]);
+        }
+
+        return [
+            'form' => $form->createView(),
         ];
     }
 }
---
+++ src/TrackerBundle/Form/UserType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace TrackerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use TrackerBundle\Entity\User;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email')
+            ->add('username', 'text')
+            ->add('fullName', 'text', [
+                'required' => false,
+            ])
+            ->add('roles', 'choice', [
+                'choices' => [
+                    User::ROLE_OPERATOR => 'ROLE_OPERATOR',
+                    User::ROLE_MANAGER => 'ROLE_MANAGER',
+                    User::ROLE_ADMIN => 'ROLE_ADMIN',
+                ],
+                'choices_as_values' => true,
+                'multiple' => true,
+            ])
+            ->add('save', 'submit', [
+                'label' => 'Save',
+                'attr' => [
+                    'class' => 'btn-lg btn-primary pull-right'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TrackerBundle\Entity\User',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
