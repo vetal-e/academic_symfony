6adb3f6b71120735658d09f839b116260a86c1c0
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-04-25 17:38:40 +0300

    OOT-1364 Fixed issue test to work with dependencies (project and user)

--- src/TrackerBundle/Tests/Controller/IssueControllerTest.php
+++ src/TrackerBundle/Tests/Controller/IssueControllerTest.php
@@ -5,6 +5,7 @@ namespace TrackerBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DomCrawler\Crawler;
+use TrackerBundle\Entity\Project;
 
 class IssueControllerTest extends WebTestCase
 {
@@ -14,93 +15,108 @@ class IssueControllerTest extends WebTestCase
     const TEST_ISSUE_PRIORITY = 'PRIORITY_HIGH';
     const TEST_ISSUE_DESCRIPTION = 'Test issue description';
 
+    // This user and project should be created from the fixture
+    const TEST_USER_NAME = 'trackerbundleOperator';
+    const TEST_USER_PASSWORD = 'trackerbundleOperator';
+    const TEST_PROJECT_CODE = 'CTE';
+
+    private $client;
+
     public function testIssueCreate()
     {
-        $client = static::createClient();
-        $client->restart();
-        $crawler = $client->request('GET', '/issue/create/');
+        $this->client = static::createClient();
+        $this->client->restart();
 
-        $code = $this->submitIssueForm($client, $crawler);
+        $crawler = $this->client->request('GET', '/project/login/');
+        $testLoginManager = $this->client->getContainer()->get('tracker.test_login.manager');
+        $crawler = $testLoginManager->doLogin($this, $this->client, self::TEST_USER_NAME, self::TEST_USER_PASSWORD);
 
-        $this->removeTestIssue($client, $code);
+        /** @var Project $project */
+        $project = $this->client->getContainer()
+            ->get('doctrine')
+            ->getRepository('TrackerBundle:Project')
+            ->findOneByCode(self::TEST_PROJECT_CODE);
+
+        $crawler = $this->client->request('GET', '/project/' . $project->getId() . '/issue/create/');
+
+        $code = $this->submitIssueForm($crawler);
+
+        $this->removeTestIssue($code);
     }
 
     /**
-     * @param Client $client
      * @param Crawler $crawler
      * @return string
      */
-    private function submitIssueForm(Client $client, Crawler $crawler)
+    private function submitIssueForm(Crawler $crawler)
     {
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertEquals(1, $crawler->filter('form[name=issue]')->count());
         $form = $crawler->filter('form[name=issue]')->form();
 
-        $uniquePart = $this->getUniquePart($client);
+        $uniquePart = $this->getUniquePart();
         $form['issue[summary]'] = self::TEST_ISSUE_SUMMARY . $uniquePart;
         $form['issue[code]'] = self::TEST_ISSUE_CODE . $uniquePart;
         $form['issue[type]'] = self::TEST_ISSUE_TYPE;
         $form['issue[priority]'] = self::TEST_ISSUE_PRIORITY;
         $form['issue[description]'] = self::TEST_ISSUE_DESCRIPTION;
-        $crawler = $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $this->client->submit($form);
+        $this->assertTrue($this->client->getResponse()->isRedirect());
 
-        $crawler = $client->followRedirect();
+        $crawler = $this->client->followRedirect();
 
         $this->assertContains(
             self::TEST_ISSUE_SUMMARY . $uniquePart,
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
         );
         $this->assertContains(
             self::TEST_ISSUE_CODE . $uniquePart,
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
         );
         $this->assertContains(
             constant('TrackerBundle\Entity\Issue::' . self::TEST_ISSUE_TYPE),
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
         );
         $this->assertContains(
             constant('TrackerBundle\Entity\Issue::' . self::TEST_ISSUE_PRIORITY),
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
         );
         $this->assertContains(
             self::TEST_ISSUE_DESCRIPTION,
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
         );
 
         return self::TEST_ISSUE_CODE . $uniquePart;
     }
 
     /**
-     * @param Client $client
      * @return string
      */
-    private function getUniquePart(Client $client)
+    private function getUniquePart()
     {
         $randomPart = (string) random_int(1000000000, 10000000000 - 1);
         $summary = self::TEST_ISSUE_SUMMARY . $randomPart;
         $code = self::TEST_ISSUE_CODE . $randomPart;
 
-        $container = $client->getContainer();
+        $container = $this->client->getContainer();
         $issueRepository = $container->get('doctrine')->getRepository('TrackerBundle:Issue');
 
         $issueBySummary = $issueRepository->findOneBySummary($summary);
         $issueByCode = $issueRepository->findOneByCode($code);
 
         if (!empty($issueBySummary) or !empty($issueByCode)) {
-            return $this->getUniquePart($client);
+            return $this->getUniquePart();
         } else {
             return $randomPart;
         }
     }
 
     /**
-     * @param Client $client
      * @param string $code
      */
-    private function removeTestIssue(Client $client, $code)
+    private function removeTestIssue($code)
     {
-        $container = $client->getContainer();
+        $container = $this->client->getContainer();
         $entityManager = $container->get('doctrine')->getManager();
         $issueRepository = $container->get('doctrine')->getRepository('TrackerBundle:Issue');
 
