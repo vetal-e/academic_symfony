4ce3376904b950732a400aab00eaebd0e23207ce
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-04-26 12:05:45 +0300

    OOT-1371 Functional tests for home page and login

--- src/TrackerBundle/Tests/Controller/DefaultControllerTest.php
+++ src/TrackerBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,17 +2,36 @@
 
 namespace TrackerBundle\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    // This user and issue should be created from the fixture
+    const TEST_USER_NAME = 'trackerbundleOperator';
+    const TEST_USER_PASSWORD = 'trackerbundleOperator';
+
+    /** @var Client $client */
+    private $client;
+
     public function testIndex()
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
+        $this->client->restart();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        // For non logged in user there is a redirect to login page
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $crawler = $this->client->followRedirect();
+
+        $testLoginManager = $this->client->getContainer()->get('tracker.test_login.manager');
+        $crawler = $testLoginManager->doLogin($this, $this->client, self::TEST_USER_NAME, self::TEST_USER_PASSWORD);
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains(
+            self::TEST_USER_NAME,
+            $this->client->getResponse()->getContent()
+        );
     }
 }
